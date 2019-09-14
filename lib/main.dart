0781f5151af871f3dc474effa11a07519f0ef52b
aabiro <aaryn.alexander@gmail.com>
2019-09-13 22:11:29 -0400

    added stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,58 @@
 import 'package:flutter/material.dart';
 
 void main() {
-   runApp(MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState () {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = ["What's your fav colour?", "Whats your favourite animal?"];
     return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("My App Title"),
-          ),
-          body: Text("This is the body text widget"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My App Title"),
         ),
+        body: Column(
+          children: [
+            Text(questions.elementAt(questionIndex)),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                print('awswer 3 chosen');
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
 //    return MaterialApp(home: Text('Hello Aaryn!'));
 //  }
 //}
